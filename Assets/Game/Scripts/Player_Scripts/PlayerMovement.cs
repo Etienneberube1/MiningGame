67392da3e522216abbf3601a59d2d9fb0bb949da
Étienne Bérubé <eti.beru@gmail.com>
2025-01-21 20:36:01 -0500

    Added player visual and Animation. player cam is no longer able to go trough wall.

--- Assets/Game/Scripts/Player_Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/Player_Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Values")]
-    [Tooltip("This Values is getting multiply by 10")]
+    [Tooltip("This Value is multiplied by 10")]
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _groundDrag;
 
@@ -12,15 +12,23 @@ public class PlayerMovement : MonoBehaviour
     private float _verticalInput;
 
 
+
     [Header("References")]
     [SerializeField] private Transform _orientation;
     private Rigidbody _rigidBody;
 
-
-    [Header("References")]
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private bool _isGrounded;
+
+    
+    [Header("Animation")]
+    [SerializeField] private Animator _animator;
+
+    [Range(0, 1f)]
+    public float _startAnimTime = 0.3f;
+
+
 
 
     private void Start()
@@ -35,6 +43,7 @@ public class PlayerMovement : MonoBehaviour
         GroundCheck();
         HandleDrag();
         SpeedControl();
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
@@ -44,8 +53,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleDrag()
     {
-        if (_isGrounded) { _rigidBody.linearDamping = _groundDrag; }
-        else { _rigidBody.linearDamping = 0; }
+        _rigidBody.linearDamping = _isGrounded ? _groundDrag : 0f;
     }
 
     private void GroundCheck()
@@ -64,16 +72,26 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 flatVelocity = new Vector3(_rigidBody.linearVelocity.x, 0f, _rigidBody.linearVelocity.z);
 
-        // Limit le Velocity if needed
+        // Limit the velocity if needed
         if (flatVelocity.magnitude > _moveSpeed)
         {
             Vector3 limitedVelocity = flatVelocity.normalized * _moveSpeed;
             _rigidBody.linearVelocity = new Vector3(limitedVelocity.x, _rigidBody.linearVelocity.y, limitedVelocity.z);
         }
     }
+
     private void HandleInput()
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void UpdateAnimation()
+    {
+        // Calculate speed from the Rigidbody's velocity
+        float speed = new Vector3(_rigidBody.linearVelocity.x, 0f, _rigidBody.linearVelocity.z).magnitude;
+
+        // Set the Blend parameter for the animation
+        _animator.SetFloat("Blend", speed / _moveSpeed, _startAnimTime, Time.deltaTime);
     }
 }
